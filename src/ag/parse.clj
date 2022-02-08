5f8381a7fb0da453c6f7ccdc8ef0e14fe6760906
Art Goldhammer <art.goldhammer@gmail.com>
2022-02-08 08:22:51 -0500

    simplified; allow symbol in def of symbol

--- src/ag/parse.clj
+++ src/ag/parse.clj
@@ -2,7 +2,7 @@
   (:require [instaparse.core :as insta])
   (:gen-class))
 
-(def query
+(def query1
   "Find in [worda wordb] from last 25 hours;\n")
 
 (def query2
@@ -19,31 +19,34 @@
   "Find in [ worda wordb
    $topic ] from last 2 hours ;\n")
 
+(def query6
+  "Define $felite [$fra Zemmour];\n")
+
 (def broken1
   "Define fra [yes no];\n")
 
 (def broken2
   "Find in [ worda wordb
-   $topic ] from last 2 hours;\n") ;; no semi
+   $topic ] from last 2 hours\n") ;; no semi
 
 (def parse
   (insta/parser
-   "S = (FINDLAST | DEF)
-    FINDLAST = <FINDIN> <LBKT> (<LWSP> SYMBOL <LWSP> | <LWSP> WORD <LWSP>)+ <RBKT>
-      <LWSP> <FROMLAST> HOURS <REST> <LWSP> <SEMI>
+   "<S> = FINDLAST | DEF
+    FINDLAST = <FINDIN> <LBKT> (SYMBOL | WORD )+ <RBKT>
+       <FROMLAST> HOURS <REST> <SEMI>
     LBKT = '['
     RBKT = ']'
     SEMI = ';\n'
-    <LWSP> = #'\\s*'
     FINDIN = 'Find in '
     FROMLAST = 'from last '
     WORD = #'[a-zA-Z0-9]+'
     SYMBOL = #'\\$[a-zA-Z0-9]+'
     HOURS = #'\\d+'
     REST = ' hours'
-    DEF = <DEFPFX> SYMBOL <LWSP> <LBKT> (<LWSP> WORD <LWSP>)+ <RBKT> <LWSP> <SEMI>
+    DEF = <DEFPFX> SYMBOL <LBKT> (SYMBOL | WORD)+ <RBKT> <SEMI>
     DEFPFX = 'Define '
     "
+   :auto-whitespace :standard
    :output-format :enlive))
 
 
@@ -54,16 +57,19 @@
 
 
 (comment
-  (parse query)
+  (parse query1)
   (parse query2)
   (parse query3)
   (parse query4)
   (parse query5)
+  (parse query6)
   (parse broken1)
   (parse broken2)
-  (subs query3 11)
+  (def queries [query1 query2 query3 query4 query5 query6])
+  (mapv parse queries)
+  (get (mapv parse queries) 5)
 
-
+  (mapv parse [broken1 broken2])
 
   (doseq [item (:content (parse query))]
     (println item)))
